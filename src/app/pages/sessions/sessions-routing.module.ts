5c65b27dfdf210199b74ccef65db9fa5efe62e08
Adrian <adriangsanchez98@gmail.com>
2020-06-19 09:57:44 +0100

    Cambio routing.

--- src/app/pages/sessions/sessions-routing.module.ts
+++ src/app/pages/sessions/sessions-routing.module.ts
@@ -3,13 +3,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SessionComponent} from './session/session.component';
 import {SessionMainComponent} from './session-main/session-main.component';
-import {CreateTomaComponent} from './create-toma/create-toma.component';
 
 const routes: Routes = [
   {path: ':id', component: SessionComponent, children: [
       {path: '', component: SessionMainComponent},
-      {path: 'crear-toma', component: CreateTomaComponent},
-
     ]
   }
 ];
